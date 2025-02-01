8f55ac25031de8bd9282834c7c485dd73f54da9b
tlikits <thanchanok.likitsinsopon@gmail.com>
2025-02-01 21:39:39 +1100

    feat: support aws profile for authenticating the cli to access dynamodb and s3

--- src/upload-manager/uploader/s3-uploader.ts
+++ src/upload-manager/uploader/s3-uploader.ts
@@ -1,10 +1,15 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import * as fs from 'fs';
 import * as path from 'path';
+import { GlobalConfiguration } from '../../global-configuration';
 import { S3UploadConfig } from '../interfaces';
 import { Uploader } from './uploader';
 
 export class S3Uploader implements Uploader<S3UploadConfig> {
+  private s3Client!: S3Client;
+
+  constructor(private readonly globalConfig: GlobalConfiguration) {}
+
   public parse(targetpath: string, filepath: string): S3UploadConfig {
     const parsed = path.parse(filepath);
     const { dir } = parsed;
@@ -19,25 +24,29 @@ export class S3Uploader implements Uploader<S3UploadConfig> {
   }
 
   public async upload(config: S3UploadConfig): Promise<void> {
+    this.ensureS3Client();
     const { filepath } = config;
-
-    console.log(`uploading to s3: ${config.bucket} - ${filepath}`);
-
-    // Configure AWS credentials
-    const s3 = new S3Client({
-      region: process.env.AWS_REGION,
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      },
-    });
+    console.debug(`Uploading to s3: ${config.bucket} - ${filepath}`);
 
     const params = {
       Bucket: config.bucket,
       Key: config.key,
       Body: fs.readFileSync(filepath),
     };
-    await s3.send(new PutObjectCommand(params));
-    console.log('Uploaded to S3');
+    await this.s3Client.send(new PutObjectCommand(params));
+    console.debug('Uploaded to S3');
+  }
+
+  public ensureS3Client(): void {
+    if (this.s3Client) {
+      return;
+    }
+    const awsProfile = this.globalConfig.getConfiguration('awsProfile');
+    if (!awsProfile) {
+      throw new Error('awsProfile is missing in the global configuration');
+    }
+    this.s3Client = new S3Client({
+      profile: awsProfile,
+    });
   }
 }
